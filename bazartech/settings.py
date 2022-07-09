3fd2f12ac6e94496601bf9df5a97fdc37795fa07
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-07-09 01:03:12 -0300

    permite login com email ou username

--- bazartech/settings.py
+++ bazartech/settings.py
@@ -216,6 +216,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTHENTICATION_BACKENDS = ( 'authentication.EmailUsernameAuth.EmailOrUsernameModelBackend', 'django.contrib.auth.backends.ModelBackend', ) 
+
 # Configure Django App for Heroku.
 
 django_heroku.settings(locals())
